38bb24f9c4e669c80e66eaa47bca556778d3d378
chrisraygill <chrisraygill@gmail.com>
2023-08-31 01:12:36 +0000

    set up firebase hosting

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDxxfO7lpxc11DJcSLQmHQ2znr0O2g0SG8",
+  authDomain: "fir-day-poetry-app.firebaseapp.com",
+  projectId: "fir-day-poetry-app",
+  storageBucket: "fir-day-poetry-app.appspot.com",
+  messagingSenderId: "493852003640",
+  appId: "1:493852003640:web:91bd7337cea3bb2ef6c5cd",
+  measurementId: "G-VM5RJWC3TC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
   const [count, setCount] = useState(0)
